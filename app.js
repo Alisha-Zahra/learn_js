01fee320d3ef856c9b0346ed2079830317cf382d
Alisha-Zahra <zahraalisha1100@gmail.com>
2026-08-31 16:39:21 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let userName = "John Doe";
+console.log("User Name:", userName);
+
+let firstName = "John";
+let lastName = "Doe";
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Full Name:", firstName + " " + lastName);
+
+let age = 30;
+console.log("Age:", age);
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+
+console.log(sum);
