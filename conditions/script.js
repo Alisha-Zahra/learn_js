d2f51b48e1fd8e87bf06c2b0419756a3eaf1099d
Alisha-Zahra <zahraalisha1100@gmail.com>
2026-09-17 15:16:40 +0500

    Added JavaScript practice

---
+++ conditions/script.js
@@ -0,0 +1,50 @@
+// let age = 20;
+// if (age <= 18) {
+//   console.log("You are young.");
+// }
+// else if (age >=18) {
+//   console.log("You are an adult.");
+// }
+// else if (age >= 65) {
+//   console.log("You are old now.");
+// }
+ 
+
+let marks = 85;
+if (marks >= 90) {
+  console.log("Grade: A");
+}
+ else if (marks >= 80) {
+  console.log("Grade: B");
+} 
+else if (marks >= 70) {
+  console.log("Grade: C");
+}
+else if (marks >= 60) {
+  console.log("Grade: D");
+}
+else {
+  console.log("Grade: F");
+}
+
+let salary =  50000;
+if (salary >= 100000) {
+  console.log("You are a high-income earner.");
+}
+  else if (salary >= 50000) {
+  console.log("You are a middle-income earner.");
+}
+else {
+  console.log("You are a low-income earner.");
+}
+
+let age = 28;
+if (age > 18){
+  console.log("you are eligible for driving license ")
+}
+ else if (age < 18){
+  console.log("Oops! your are very young now... we can't make your license")
+ }
+else if(age == 18){
+  console.log("just wait for one year...")
+}
